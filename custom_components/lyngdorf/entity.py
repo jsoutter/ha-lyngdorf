fa3e70c6432d76c66fac7dae7f2bd1ddc0eb61cc
test <test@test.com>
2025-11-27 22:57:17 +0000

    Update config flow

--- custom_components/lyngdorf/entity.py
+++ custom_components/lyngdorf/entity.py
@@ -1,42 +1,97 @@
-"""Base Entity for Lyngdorf."""
+"""Base entity for Lyngdorf integration."""
 
 from __future__ import annotations
 
+import logging
 from typing import TYPE_CHECKING
 
-from homeassistant.const import CONF_NAME
+from homeassistant.const import CONF_IP_ADDRESS, CONF_MODEL, CONF_NAME, CONF_PORT
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.device_registry import DeviceInfo
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
+
 
 from .const import DOMAIN as LYNGDORF_DOMAIN
 from .const import NAME as LYNGDORF_NAME
+from .pylyngdorf.const import DeviceModel, DEFAULT_PORT
+from .pylyngdorf.exceptions import LyngdorfNetworkError, LyngdorfTimoutError
+from .pylyngdorf.lyngdorf import Lyngdorf
 
 if TYPE_CHECKING:
     from homeassistant.config_entries import ConfigEntry
-    from lyngdorf.device import Receiver
+
+_LOGGER = logging.getLogger(__name__)
+
+type LyngdorfConfigEntry = ConfigEntry[LyngdorfCoordinator]
 
 
-class LyngdorfEntity(Entity):
+class LyngdorfCoordinator(DataUpdateCoordinator[None]):
+    """Define the Lyngdorf integration coordinator."""
+
+    def __init__(self, hass: HomeAssistant, config_entry: LyngdorfConfigEntry) -> None:
+        """Set up the coordinator and set in config_entry."""
+        super().__init__(hass, _LOGGER, config_entry=config_entry, name=LYNGDORF_DOMAIN)
+
+        self.entry_id = config_entry.entry_id
+        self.name = config_entry.data[CONF_NAME]
+        self.host = config_entry.data[CONF_IP_ADDRESS]
+        self.port = config_entry.data.get(CONF_PORT) or DEFAULT_PORT
+
+        try:
+            self.model = DeviceModel(config_entry.options.get(CONF_MODEL))
+        except ValueError:
+            self.model = DeviceModel.MP60
+
+        self.receiver: Lyngdorf = Lyngdorf.create(
+            self.host,
+            self.port,
+            device_model=self.model,
+        )
+
+    async def async_setup(self) -> None:
+        """Set up the coordinator; connect to the host; and retrieve initial data."""
+        # Connect to the device
+        try:
+            await self.receiver.async_connect()
+        except (LyngdorfTimoutError, LyngdorfNetworkError) as err:
+            msg = f"Unable to connect to {self.host}: {err}"
+            raise ConfigEntryNotReady(msg) from err
+
+        # Set notification callback
+        self.receiver.set_notification_callback(self._notify_callback)
+
+    async def _async_update_data(self):
+        """No polling needed."""
+        return self.data
+
+    def _notify_callback(self) -> None:
+        """Handle a notification."""
+        self.async_update_listeners()
+
+    async def async_shutdown(self) -> None:
+        """Disconnect from the device."""
+        await self.receiver.async_disconnect()
+        await super().async_shutdown()
+
+
+class LyngdorfEntity(CoordinatorEntity[LyngdorfCoordinator]):
     """Defines a base Lyngdorf entity."""
 
     _attr_has_entity_name = True
     _attr_should_poll = False
 
-    def __init__(self, receiver: Receiver, config_entry: ConfigEntry) -> None:
+    def __init__(self, coordinator: LyngdorfCoordinator) -> None:
         """Initialize entity."""
-        self._receiver = receiver
-        self._attr_unique_id = config_entry.entry_id
+        super().__init__(coordinator)
+        self._receiver = coordinator.receiver
+        self._attr_unique_id = coordinator.entry_id
         self._attr_device_info = DeviceInfo(
-            identifiers={(LYNGDORF_DOMAIN, config_entry.entry_id)},
-            name=config_entry.data[CONF_NAME],
+            identifiers={(LYNGDORF_DOMAIN, coordinator.entry_id)},
+            name=coordinator.name,
             manufacturer=LYNGDORF_NAME,
-            model=config_entry.data[CONF_NAME],
+            model=coordinator.model.value,
         )
-
-    async def async_added_to_hass(self) -> None:
-        """Notify of addition to haas."""
-        self._receiver.register_notification_callback(self.async_write_ha_state)
-
-    async def async_will_remove_from_hass(self) -> None:
-        """Notify of removal from haas."""
-        self._receiver.un_register_notification_callback(self.async_write_ha_state)
---
+++ custom_components/lyngdorf/pylyngdorf/lyngdorf.py
@@ -0,0 +1,310 @@
+#!/usr/bin/env python3
+"""
+Module implements the interface to Lyngdorf processors.
+
+:license: MIT, see LICENSE for more details.
+"""
+
+from typing import TypeVar
+
+import attr
+
+from .const import (
+    DEFAULT_PORT,
+    DEVICE_PROTOCOLS,
+    MIN_VOLUME_DB,
+    DeviceModel,
+    LyngdorfCommands,
+)
+from .device import LyngdorfDevice
+
+T = TypeVar("T", bound="Lyngdorf")
+
+
+@attr.define()
+class Lyngdorf(LyngdorfDevice):
+    """Implements a class with device information of the processor."""
+
+    _host: str = attr.field(converter=str)
+    _port: int = attr.field(converter=int)
+    _timeout: float = attr.field(converter=float)
+    _device_model: DeviceModel | None = attr.field(default=None)
+
+    def __new__(
+        cls: type[T],
+    ) -> T:
+        raise RuntimeError("Use Lyngdorf.create() instead of direct instantiation")
+
+    @classmethod
+    def create(
+        cls: type[T],
+        host: str,
+        port: int = DEFAULT_PORT,
+        timeout: float = 2.0,
+        device_model: DeviceModel | None = None,
+    ) -> T:
+        instance = object.__new__(cls)
+        cls.__init__(instance, host, port, timeout, device_model)
+        return instance
+
+    def __attrs_post_init__(self) -> None:
+        """Initialise attributes."""
+        self._api.host = self._host
+        self._api.port = self._port
+        self._api.timeout = self._timeout
+
+        if self._device_model is not None and self._device_model in DEVICE_PROTOCOLS:
+            self._api.device_protocol = DEVICE_PROTOCOLS[self._device_model]
+
+    async def async_send_commands(
+        self, *commands: str, skip_confirmation: bool = False
+    ) -> None:
+        """Send commands to the processor."""
+        await self._api.async_send_commands(
+            *commands, skip_confirmation=skip_confirmation
+        )
+
+    def send_commands(self, *commands: str) -> None:
+        """Send commands to the processor."""
+        self._api.send_commands(*commands)
+
+    async def async_connect(self) -> None:
+        """Connect to the interface of the processor."""
+        await self.register_callbacks()
+        await self._api.async_connect()
+
+    async def async_disconnect(self) -> None:
+        """Disconnect from the interface of the processor."""
+        await self._api.async_disconnect()
+
+    ##############
+    # Properties #
+    ##############
+    @property
+    def host(self) -> str:
+        """Return the host of the device as string."""
+        return self._host
+
+    @property
+    def model(self) -> DeviceModel | None:
+        """Return the model of the device as string."""
+        return self._model
+
+    @property
+    def multichannel(self) -> bool:
+        """Boolean if device is multichannel processor."""
+        return self._api.device_protocol.multichannel
+
+    @property
+    def power(self) -> bool | None:
+        """Boolean if device is currently on."""
+        return self._power
+
+    @property
+    def volume(self) -> float | None:
+        """Return the volume of the device as float."""
+        return self._volume
+
+    @property
+    def volume_percent(self) -> float | None:
+        """Return the volume percent of the device as float."""
+        return self._volume_percent
+
+    @property
+    def muted(self) -> bool | None:
+        """Boolean if volume is currently muted."""
+        return self._muted
+
+    @property
+    def max_volume(self) -> float:
+        """Return the maximum volume of the device as float."""
+        return self._max_volume
+
+    @property
+    def sources(self) -> list[str]:
+        """Return the sources of the device as list."""
+        return self._sources.get_all()
+
+    @property
+    def source(self) -> str | None:
+        """Return the current source of the device."""
+        return self._source
+
+    @property
+    def stream_type(self) -> str | None:
+        """Return the current stream type of the device as string."""
+        return self._stream_type
+
+    @property
+    def voicings(self) -> list[str]:
+        """Return the voicings of the device as list."""
+        return self._voicings.get_all()
+
+    @property
+    def voicing(self) -> str | None:
+        """Return the current voicing of the device."""
+        return self._voicing
+
+    @property
+    def focus_positions(self) -> list[str]:
+        """Return the focus positions of the device as list."""
+        return self._focus_positions.get_all()
+
+    @property
+    def focus_position(self) -> str | None:
+        """Return the current focus position of the device."""
+        return self._focus_position
+
+    @property
+    def audio_modes(self) -> list[str]:
+        """Return the audio modes of the device as list."""
+        return self._audio_modes.get_all()
+
+    @property
+    def audio_mode(self) -> str | None:
+        """Return the current audio mode of the device."""
+        return self._audio_mode
+
+    @property
+    def audio_input(self) -> str | None:
+        """Return the current audio input of the device as string."""
+        return self._audio_input
+
+    @property
+    def audio_type(self) -> str | None:
+        """Return the current audio type of the device as string."""
+        return self._audio_type
+
+    @property
+    def video_input(self) -> str | None:
+        """Return the current video input of the device as string."""
+        return self._video_input
+
+    @property
+    def video_type(self) -> str | None:
+        """Return the current video type of the device as string."""
+        return self._video_type
+
+    @property
+    def video_output(self) -> str | None:
+        """Return the main video output of the device as string."""
+        return self._video_output
+
+    @property
+    def min_lipsync(self) -> int:
+        """Return the minimum lipsync of the device as int."""
+        return self._min_lipsync
+
+    @property
+    def max_lipsync(self) -> int:
+        """Return the maximum lipsync of the device as int."""
+        return self._max_lipsync
+
+    @property
+    def lipsync(self) -> int:
+        """Return the lipsync of the device as int."""
+        return self._lipsync
+
+    @property
+    def dts_dialog_available(self) -> float | None:
+        """Boolean if dts dialog is available."""
+        return self._dts_dialog_available
+
+    @property
+    def dts_dialog(self) -> float | None:
+        """Return the dts dialog value of the device as float."""
+        return self._dts_dialog
+
+    @property
+    def loudness(self) -> float | None:
+        """Boolean if loudness is enabled."""
+        return self._loudness
+
+    ##########
+    # Setter #
+    ##########
+    async def async_power_on(self) -> None:
+        """Turn on processor."""
+        cmd = self._api.device_protocol.commands.get_command(LyngdorfCommands.POWER_ON)
+        await self._api.async_send_commands(cmd)
+
+    async def async_power_off(self) -> None:
+        """Turn off processor."""
+        cmd = self._api.device_protocol.commands.get_command(LyngdorfCommands.POWER_OFF)
+        await self._api.async_send_commands(cmd)
+
+    async def async_volume_up(self) -> None:
+        """Increase volume."""
+        cmd = self._api.device_protocol.commands.get_command(LyngdorfCommands.VOLUME_UP)
+        await self._api.async_send_commands(cmd, skip_confirmation=True)
+
+    async def async_volume_down(self) -> None:
+        """Decrease volume."""
+        cmd = self._api.device_protocol.commands.get_command(
+            LyngdorfCommands.VOLUME_DOWN
+        )
+        await self._api.async_send_commands(cmd, skip_confirmation=True)
+
+    async def async_set_volume(self, volume: float) -> None:
+        "Set processor volume."
+        if volume < MIN_VOLUME_DB or volume > self.max_volume:
+            raise ValueError(f"Invalid volume: {volume}")
+
+        cmd = self._api.device_protocol.commands.get_command(
+            LyngdorfCommands.VOLUME, volume * 10
+        )
+        await self._api.async_send_commands(cmd)
+
+    async def async_set_volume_percent(self, volume: float) -> None:
+        "Set processor volume percent."
+        db = self._linear_to_log_interpolated_db(volume)
+        await self.async_set_volume(db)
+
+    async def async_mute(self, mute: bool) -> None:
+        """Mute processor."""
+        cmd = self._api.device_protocol.commands.get_command(
+            LyngdorfCommands.MUTE_ON if mute else LyngdorfCommands.MUTE_OFF
+        )
+        await self._api.async_send_commands(cmd)
+
+    async def async_set_source(self, source: str) -> None:
+        "Set input source of processor."
+        if (index := self._sources.get_by_value(source)) is not None:
+            cmd = self._api.device_protocol.commands.get_command(
+                LyngdorfCommands.SOURCE, index
+            )
+            await self._api.async_send_commands(cmd)
+
+    async def async_set_voicing(self, voicing: str) -> None:
+        "Set voicing of processor."
+        if (index := self._voicings.get_by_value(voicing)) is not None:
+            cmd = self._api.device_protocol.commands.get_command(
+                LyngdorfCommands.VOICING, index
+            )
+            await self._api.async_send_commands(cmd)
+
+    async def async_set_focus_position(self, focus_position: str) -> None:
+        "Set focus position of processor."
+        if (index := self._focus_positions.get_by_value(focus_position)) is not None:
+            cmd = self._api.device_protocol.commands.get_command(
+                LyngdorfCommands.FOCUS_POSITION, index
+            )
+            await self._api.async_send_commands(cmd)
+
+    async def async_set_audio_mode(self, audio_mode: str) -> None:
+        "Set audio mode of processor."
+        if (index := self._audio_modes.get_by_value(audio_mode)) is not None:
+            cmd = self._api.device_protocol.commands.get_command(
+                LyngdorfCommands.AUDIO_MODE, index
+            )
+            await self._api.async_send_commands(cmd)
+
+    async def async_set_lipsync(self, lipsync: float) -> None:
+        "Set processor lipsync."
+        if lipsync < self.min_lipsync or lipsync > self.max_lipsync:
+            raise ValueError(f"Invalid lipsync: {lipsync}")
+
+        cmd = self._api.device_protocol.commands.get_command(
+            LyngdorfCommands.LIPSYNC, lipsync
+        )
+        await self._api.async_send_commands(cmd)
